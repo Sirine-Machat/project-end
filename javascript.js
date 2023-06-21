8db27324f5be8eea4061889c667ea95941a97157
sirine <machatsirine71@gmail.com>
2023-06-21 10:20:39 +0200

    project end

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let search =document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+search.classList.toggle('active');
+}
+
+function validateForm(e) {
+    e.preventDefault()
+    var name = document.loginForm.name.value
+    var password = document.loginForm.password.value
+   if (name.length==0)
+    return alert(`name is required`)
+   if (password.length<5)
+    return alert(`password length should more than 5`)
+   }
