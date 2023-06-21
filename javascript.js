3458649f0a5375d6046e12de0a5700fa7cab930a
sirine <machatsirine71@gmail.com>
2023-06-21 19:26:51 +0200

    project-end

--- javascript.js
+++ javascript.js
@@ -8,8 +8,16 @@ function validateForm(e) {
     e.preventDefault()
     var name = document.loginForm.name.value
     var password = document.loginForm.password.value
+    var number=document.loginForm.number.value
    if (name.length==0)
-    return alert(`name is required`)
+    return alert(`name is required`);
    if (password.length<5)
-    return alert(`password length should more than 5`)
+    return alert(`password length should more than 5`);
    }
+   const stars=document.getElementsByClassName('bx bxs-star');
+
+for(let i of stars) {
+    i.addEventListener('click', function() {
+        i.classList.toggle('glod');
+    })
+}
